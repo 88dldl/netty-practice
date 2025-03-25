cff0de42b019c286445e4be15fc57e70ffd1b912
88dldl <chltnrgml0999@gmail.com>
2025-03-25 19:18:17 +0900

    feat: HTTP 코덱을 사용해 서버 요청 처리

---
+++ src/main/java/netty/codec/HttpHelloWorldServerInitializer.java
@@ -0,0 +1,27 @@
+package netty.codec;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.ssl.SslContext;
+
+public class HttpHelloWorldServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SslContext sslCtx;
+
+    public HttpHelloWorldServerInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        if (sslCtx != null) {
+            pipeline.addLast(sslCtx.newHandler(ch.alloc()));
+        }
+
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new HttpHelloWorldServerHandler());
+    }
+}
